9387156576d5066293ae10fa36ababd1347ffbc6
MDaugaardDK <48121840+MDaugaardDK@users.noreply.github.com>
2020-01-26 11:17:54 +0100

    Add files via upload

---
+++ Get-licensdetail_ADGroup.ps1
@@ -0,0 +1,12 @@
+﻿$ADGroupMembers = Get-ADGroupMember -identity "ADGroup" -Recursive | Get-ADUser -Properties UserPrincipalName | Select UserPrincipalName
+
+$username = "user@domain.com"
+$pwdTxt = Get-Content "C:\Path\Encrypted.txt"
+$securePwd = $pwdTxt | ConvertTo-SecureString -Key (1..16)
+$credObject = New-Object System.Management.Automation.PSCredential -ArgumentList $username, $securePwd
+Connect-AzureAD -Credential $credObject
+
+ForEach ($Member in $ADGroupMembers) {
+$member.UserPrincipalName
+Get-AzureADUserLicenseDetail -ObjectId "$($Member.UserPrincipalName)" | select SkuPartNumber | fl *
+}
